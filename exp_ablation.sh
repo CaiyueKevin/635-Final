2fc5e99df158c1ac80637d5fc2b13cc6852cc90c
unknown <hgao@umass.edu>
2024-12-17 04:21:57 +0800

    Add project files from iotdi22-mmfl-main

---
+++ exp_ablation.sh
@@ -0,0 +1,15 @@
+python src/main.py --config ./config/opp/ablation/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/opp/ablation/A30_B30_AB0_label_B_test_A
+python src/main.py --config ./config/ur_fall/ablation/acce_depth/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/ur_fall/ablation/acce_depth/A30_B30_AB0_label_B_test_A
+python src/main.py --config ./config/ur_fall/ablation/rgb_depth/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/ur_fall/ablation/rgb_depth/A30_B30_AB0_label_B_test_A
+python src/main.py --config ./config/mhealth/ablation/acce_gyro/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/mhealth/ablation/acce_gyro/A30_B30_AB0_label_B_test_A
+python src/main.py --config ./config/mhealth/ablation/acce_mage/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/mhealth/ablation/acce_mage/A30_B30_AB0_label_B_test_A
+python src/main.py --config ./config/mhealth/ablation/gyro_mage/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/mhealth/ablation/gyro_mage/A30_B30_AB0_label_B_test_A
+
+python src/main.py --config ./config/ur_fall/ablation/acce_rgb/A30_B30_AB0_label_A_test_B
+python src/main.py --config ./config/ur_fall/ablation/acce_rgb/A30_B30_AB0_label_B_test_A
